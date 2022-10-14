41c07896ea9336d57ce2bed9b0634c4dfe6d26a4
Lillian <lillianvictor860@gmail.com>
2022-10-13 17:52:13 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - prints exactly with proper grammar, but the outcome is a piece of art,
+ * main - prints exactly with proper grammar
  *
- * Description:'printf'
+ * Description:'printf()'
  *
  * return: Always 0 (Success)
  */
