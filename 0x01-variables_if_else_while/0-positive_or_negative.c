3febc3dcde29beb9cce1d365828bb61563c40440
LIllian <84461661+Lilly-Vic@users.noreply.github.com>
2022-10-14 13:58:47 -0700

    variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,13 +5,13 @@
 /**
  * main - assign a random number to the variable n each time it is executed
  * print whether the number stored in the variable n is positive or negative
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int n;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
