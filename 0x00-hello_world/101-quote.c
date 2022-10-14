5c844685813c38c8c930cb4a6367f8e947698bb6
Lillian <lillianvictor860@gmail.com>
2022-10-13 17:32:15 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'writing'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
