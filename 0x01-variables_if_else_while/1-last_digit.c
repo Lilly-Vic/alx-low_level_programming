f8e84fe3507b95fa1404b5e8ea61571bbbe4ae22
LIllian <84461661+Lilly-Vic@users.noreply.github.com>
2022-10-14 15:36:58 -0700

    variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * Description: assign a random number to the variable n each time it is executed and print the last digit of the number stored in the variable n
+ * Description: assign a random number to the variable and print the last digit stored in the variable
  * Return: Always 0 (Success)
  */
 
