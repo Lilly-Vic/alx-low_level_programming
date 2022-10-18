cc9b3c24a68cb6cccb34be602ef1d0a19bf6a1ce
LIllian <84461661+Lilly-Vic@users.noreply.github.com>
2022-10-18 14:35:37 -0700

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,14 @@
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
  *
- * Return: void
+ * Return: Always 0 (Success)
  */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
 
 void print_alphabet(void)
 {
