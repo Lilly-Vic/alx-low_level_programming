55a1266f515d186a43bdb271bd020465c7e11cac
LIllian <84461661+Lilly-Vic@users.noreply.github.com>
2022-10-18 18:46:21 -0700

    nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,8 +6,12 @@
  * Return: Always 0 (Success)
  */
 
-int main(void){
-	int n, f1 = 1, f2 = 2, next = 0;
+int main(void)
+{
+	long int n, f1, f2, next;
+	
+	f1 = 1;
+	f2 = 2;
 
 	for (n = 1; n <= 50; ++n)
 	{
