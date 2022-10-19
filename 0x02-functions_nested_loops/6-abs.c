c2c5435916084be01fd0886c98e2d4d893c7a27e
LIllian <84461661+Lilly-Vic@users.noreply.github.com>
2022-10-18 17:21:36 -0700

    nested loop

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs(int) - computes the absolute value of an integer
+ * _abs - computes the absolute value of an integer
  * @r: integer input
  * Return; Always 0 (Success)
  */
