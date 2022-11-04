9017b77f433d45792906052b5ed7d15a6677bf45
Lillian <lillianvictor860@gmail.com>
2022-11-04 08:46:00 -0700

    argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv:array of arguments
+ * Return: number of arguments
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: return 1 if error
+ * and 0 if no num is passed
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
