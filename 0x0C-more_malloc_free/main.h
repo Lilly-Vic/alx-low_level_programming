7ef732bf40696e6d73ce8e2bb7222258edb6cb97
LIllian <84461661+Lilly-Vic@users.noreply.github.com>
2022-11-10 11:35:33 -0800

    more malloc, free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
