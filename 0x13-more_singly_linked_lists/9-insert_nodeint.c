598aba58eb01e88559f0fd3d9eb29edc685d2c2d
LIllian <84461661+Lilly-Vic@users.noreply.github.com>
2022-12-01 13:48:48 -0800

    linked lists 2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,38 +10,33 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	listint_t *current_node = *head;
-	listint_t *new_node;
+	unsigned int count = 0;
+	listint_t *current_node, *new_node;
+
+	if (*head == NULL && idx != 0)
+		return (NULL);
+
+	if (idx != 0)
+	{
+		current_node = *head;
+		for (; count < idx - 1 && current_node != NULL; count++)
+			current_node = current_node->next;
+		if (current_node == NULL)
+			return (NULL);
+	}
+	
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
 
 	if (idx == 0)
 	{
-		new_node = malloc(sizeof(listint_t));
-
-		if (!new_node)
-			return (NULL);
-		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-
-	while (current_node && ((i + 1) != idx))
-	{
-		current_node = current_node->next;
-		i++;
-	}
-
-	if ((i + 1) == idx)
-	{
-		new_node = malloc(sizeof(listint_t));
-
-		if (!new_node)
-			return (NULL);
-		new_node->n = n;
-		new_node->next = current_node->next;
-		current_node->next = new_node;
-		return (new_node);
-	}
+	new_node->next = current_node->next;
+	current_node->next = new_node;
 	return (NULL);
 }
